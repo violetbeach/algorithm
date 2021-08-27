789407a39c1761b1770a40c853849bd93071fef5
Lee Jaeheon <ti6419@gmail.com>
2021-08-27 15:50:07 +0900

    Solve: 스킬체크 레벨1 - 내적

---
+++ skillcheck/level1/DotProduct.java
@@ -0,0 +1,22 @@
+package skillcheck.level1;
+
+public class DotProduct {
+	
+	public static int solution(int[] a, int[] b) {
+		int answer = 0;
+		
+		for(int i=0; i<a.length; i++) {
+			answer += a[i]*b[i];
+		}
+		
+		return answer;
+    }
+
+	public static void main(String[] args) {
+		int[] a = {1,2,3,4};
+		int[] b = {-3,-1,0,2};
+		System.out.println(solution(a,b));	
+	}
+	
+	
+}
