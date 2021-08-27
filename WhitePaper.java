1e078ec0f22fc0e0c6e8866cf92bb14373cade32
Lee Jaeheon <ti6419@gmail.com>
2021-08-27 23:45:04 +0900

    feat: Add 풀이 백지

---
+++ WhitePaper.java
@@ -0,0 +1,14 @@
+
+public class WhitePaper {
+
+	public static int solution(int solution) {
+		int answer = 0;
+		
+		return answer;
+    }
+
+	public static void main(String[] args) {
+		
+		System.out.println(solution(0));	
+	}
+}
