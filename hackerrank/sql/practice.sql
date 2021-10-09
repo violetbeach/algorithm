46a40747a84f1463002cbceda760a230e4a8a522
Lee Jaeheon <ti6419@gmail.com>
2021-10-09 19:54:24 +0900

    Solve: SQL - hacker rank sql 11~15

--- hackerrank/sql/practice.sql
+++ hackerrank/sql/practice.sql
@@ -29,3 +29,17 @@ SELECT count(1) - count(distinct CITY) from station;
 (select CITY, length(CITY) from STATION order by length(CITY), CITY limit 1)
 UNION
 (select CITY, length(CITY) from STATION order by length(CITY) DESC, CITY limit 1);
+11. Weather Observation Station 6 -- 1. substring, 2. regexp, 3. LIKE OR
+select distinct city from station where city regexp '^[aeiou]';
+
+12. Weather Observation Station 7
+select distinct city from station where city regexp '[aeiou]$';
+
+13. Weather Observation Station 8
+select distinct city from station where city regexp '^[aeiou].*[aeiou]$';
+
+14. Weather Observation Station 9
+select distinct city from station where city regexp '^[^aeiou]';
+
+15. Weather Observation Station 10
+select distinct city from station where city regexp '[^aeiou]$';
