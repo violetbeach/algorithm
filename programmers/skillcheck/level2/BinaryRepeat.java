204c3a568724738cd58da663c1df861db852f278
Jerry(이재헌) <ti6419@gmail.com>
2022-09-27 22:15:56 +0900

    solve: level2 - 이진 변환 반복하기

---
+++ programmers/skillcheck/level2/BinaryRepeat.java
@@ -0,0 +1,27 @@
+package skillcheck.level2;
+
+/*
+ * 이진 변환 반복하기
+ *
+ * */
+
+public class BynaryRepeat {
+
+    public int[] solution(String s) {
+        int[] answer = new int[2];
+
+        while (s.length() > 1) {
+            int cntOne = 0;
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '0') answer[1]++;
+                else cntOne++;
+            }
+
+            s = Integer.toBinaryString(cntOne);
+            answer[0]++;
+        }
+
+        return answer;
+    }
+
+}
