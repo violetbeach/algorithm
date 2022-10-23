9bb66c554dfbd8c422880cfeb0d7f2987908963e
Jerry(이재헌) <ti6419@gmail.com>
2022-10-24 08:29:48 +0900

    feat: 이분 탐색(Binary Search) Base 알고리즘 추가

---
+++ basicreference/BinarySearch.java
@@ -0,0 +1,23 @@
+// 이분 탐색
+
+public class BinarySearch {
+
+    int binarySearch(int key, int low, int high) {
+        int mid;
+
+        while(low <= high) {
+            mid = (low + high) / 2;
+
+            if(key == arr[mid]) {
+                return mid;
+            } else if(key < arr[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+
+}
