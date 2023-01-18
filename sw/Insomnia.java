14a52d1320c53a257e2be23518075563417ffe6b
Jerry(이재헌) <ti6419@gmail.com>
2023-01-19 08:48:48 +0900

    solve: SW - Insomnia

---
+++ sw/Insomnia.java
@@ -0,0 +1,65 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+import java.io.*;
+import java.util.*;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Insomnia
+{
+    public static void main(String args[]) throws Exception
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T;
+        T=Integer.parseInt(br.readLine());
+
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            StringTokenizer st;
+            st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int cur = 0;
+            int validate = 0;
+            int multi = 0;
+            while(validate != (1 << 10) -1) {
+                //새로운 숫자 만들기.
+                cur = N * (multi++);
+                //검증하기
+                while(cur > 0) {
+                    int temp = cur % 10;
+                    validate |= (1 << temp);
+                    cur /= 10;
+                }
+            }
+            System.out.println("#"+ test_case+ " "+ N * (multi-1));
+        }
+    }
+}
