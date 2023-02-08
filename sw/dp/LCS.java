05c59f68768a3414e03b8f020f96bfddc544f4e1
Jerry(이재헌) <ti6419@gmail.com>
2023-02-08 12:51:01 +0900

    solve: SW - (완료) 최장 공통 부분 수열

--- sw/dp/LCS.java
+++ sw/dp/LCS.java
@@ -64,6 +64,18 @@ class LCS
 
             int[][] dp = new int[s1.length()][s2.length()];
 
+            for(int i = 0; i < s2.length(); i++) {
+                if(s1.charAt(0) == s2.charAt(i)) {
+                    dp[0][i] = 1;
+                }
+            }
+
+            for(int i = 0; i < s1.length(); i++) {
+                if(s2.charAt(0) == s1.charAt(i)) {
+                    dp[i][0] = 1;
+                }
+            }
+
             for(int i = 0; i < s1.length(); i++) {
 
                 for(int j = 0; j < s2.length(); j++) {
@@ -77,10 +89,9 @@ class LCS
                     }
                     if(s1.charAt(i) == s2.charAt(j)) {
                         // TODO 여기 구현
-                        /*if(i > 0 && j > 0) {
+                        if(i > 0 && j > 0) {
                             dp[i][j] = dp[i-1][j-1] + 1;
-                        }*/
-                        // dp[i][j] = Math.max(first, second) + 1;
+                        }
                     } else {
                         dp[i][j] = Math.max(first, second);
                     }
