24c3b69d7e448cc675d2694bcc3b7476070bb43a
Jerry(이재헌) <ti6419@gmail.com>
2023-02-27 08:50:47 +0900

    solve: SW - 촛불 이벤트 (이진 탐색)

---
+++ sw/binary/Candlelight.java
@@ -0,0 +1,105 @@
+package binary;
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;
+// double b = 1.0;
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.util.Scanner;
+
+/*
+* 촛불 이벤트
+*
+* 중요한 것은 이분 탐색을 할 때 동등 여부를 초반에 판단하는 것이다.
+*
+* (로직이 매우 간단해지고 오류가 사라진다.)
+*
+* 추가로 무한 루프 여부를 판단해야한다. (while 조건에 맞는 내부 <= >= 조건이 필요함)
+* */
+
+class Candlelight
+{
+    public static void main(String args[]) throws Exception
+    {
+		/*
+		   아래의 메소드 호출은 앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
+		   여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
+		   이 코드를 프로그램의 처음 부분에 추가하면 이후 입력을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
+		   따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 메소드를 사용하셔도 좋습니다.
+		   단, 채점을 위해 코드를 제출하실 때에는 반드시 이 메소드를 지우거나 주석 처리 하셔야 합니다.
+		 */
+        //System.setIn(new FileInputStream("res/input.txt"));
+
+		/*
+		   표준입력 System.in 으로부터 스캐너를 만들어 데이터를 읽어옵니다.
+		 */
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+		/*
+		   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+		*/
+
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+
+            long N = sc.nextLong();
+
+            long start = 1;
+            long end = 2000000000;
+            long mid = (start + end) / 2;
+
+            long value = 0;
+            while(start <= end) {
+                value = mid * (mid + 1) / 2;
+
+                if(N == value) {
+                    System.out.printf("#%d %d\n", test_case, mid);
+                    break;
+                }
+                if(N <= value)  {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+
+                mid = (start + end) / 2;
+            }
+
+            if(N != value) {
+                System.out.printf("#%d %d\n", test_case, -1);
+            }
+
+
+
+            /////////////////////////////////////////////////////////////////////////////////////////////
+			/*
+				 이 부분에 여러분의 알고리즘 구현이 들어갑니다.
+			 */
+            /////////////////////////////////////////////////////////////////////////////////////////////
+
+        }
+    }
+}
